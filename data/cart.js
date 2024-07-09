3faee0300533b7c2515f65b43882902619d19fde
Evil609 <saraessan3@gmail.com>
2024-07-09 11:25:30 +0300

    use promises instead of call back

--- data/cart.js
+++ data/cart.js
@@ -102,3 +102,15 @@ export function updateCheckoutQuantity() {
 
   return cartQuantity
 }
+export function loadCart(fun){
+  const xhr =  new XMLHttpRequest();
+
+  xhr.addEventListener('load', ()=>{
+    
+    console.log(xhr.response)
+    fun();
+  })
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send()
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,47 @@
+import { loadCart } from "../data/cart.js";
 import { loadProducts } from "../data/products.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import "../data/car.js";
 // import '../data/backend-practice.js';
-loadProducts(() =>{
-    renderOrderSummary();
+
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(()=>{
+      resolve();
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(()=>{
+      resolve();
+    }); 
+  })
+]).then(()=>{
+  renderOrderSummary();
   renderPaymentSummary();
 })
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+}).then(() => {
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  }).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
+/*
+loadProducts(() =>{
+  loadCart(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+  })
+})
+*/
